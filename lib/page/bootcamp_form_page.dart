7b2b0560c1ce9cb34f4e7759d7fdec16883dac28
Rafael ThaytoTani <rafael.thaytotani@avanade.com>
2021-06-11 22:31:00 -0300

    :sparkles: feat(form): saving form data into sqlite

--- lib/page/bootcamp_form_page.dart
+++ lib/page/bootcamp_form_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ps13si/db/bootcamp_operations.dart';
 import 'package:ps13si/model/bootcamp.dart';
 
 class BootcampFormPage extends StatelessWidget {
-  const BootcampFormPage({Key? key}) : super(key: key);
+  var bootcampOperations = BootcampOperations();
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, String?>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +18,21 @@ class BootcampFormPage extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.save),
             onPressed: () {
-              Navigator.of(context).pop();
+              final isValid = _formKey.currentState!.validate();
+
+              if (isValid) {
+                _formKey.currentState!.save();
+                bootcampOperations.create(Bootcamp(
+                  nome: _formData[BootcampFields.nome] ?? '',
+                  descricao: _formData[BootcampFields.descricao] ?? '',
+                  nomeEmpresa: _formData[BootcampFields.nomeEmpresa] ?? '',
+                  especialidade: _formData[BootcampFields.especialidade] ?? '',
+                  localizacao: _formData[BootcampFields.localizacao] ?? '',
+                  candidatosMax:
+                      int.parse(_formData[BootcampFields.candidatosMax] ?? '0'),
+                ));
+                Navigator.of(context).pop();
+              }
             },
           )
         ],
@@ -22,27 +40,46 @@ class BootcampFormPage extends StatelessWidget {
       body: Padding(
           padding: EdgeInsets.all(15),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Nome'),
+                  validator: (value) {},
+                  onSaved: (value) => _formData[BootcampFields.nome] = value,
                 ),
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Descrição'),
+                  validator: (value) {},
+                  onSaved: (value) =>
+                      _formData[BootcampFields.descricao] = value,
                 ),
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Nome Empresa'),
+                  validator: (value) {},
+                  onSaved: (value) =>
+                      _formData[BootcampFields.nomeEmpresa] = value,
                 ),
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Especialidade'),
+                  validator: (value) {},
+                  onSaved: (value) =>
+                      _formData[BootcampFields.especialidade] = value,
                 ),
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Localização'),
+                  validator: (value) {},
+                  onSaved: (value) =>
+                      _formData[BootcampFields.localizacao] = value,
                 ),
                 TextFormField(
-                    decoration:
-                        InputDecoration(labelText: 'Número max. de candidatos'),
-                    keyboardType: TextInputType.number),
+                  decoration:
+                      InputDecoration(labelText: 'Número max. de candidatos'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {},
+                  onSaved: (value) =>
+                      _formData[BootcampFields.candidatosMax] = value,
+                ),
               ],
             ),
           )),
